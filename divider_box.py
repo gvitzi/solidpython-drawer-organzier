f09f85d72ed941e7e96d07fd3e502a1562c11411
Gil Vitzinger <gilvitzi@gmail.com>
2022-06-01 11:18:49 +0200

    Added argument praser

--- divider_box.py
+++ divider_box.py
@@ -1,14 +1,15 @@
 from solid import *
 from solid.utils import * 
+import sys
 
 default_height=30
 
-x = 100
+x = 200
 y = 200
 z = 30
 
-divider_spacing_y = 200
-divider_spacing_x = 200
+divider_spacing_y = 100
+divider_spacing_x = 50
 
 corner_radius = 2
 corner_radius_segments = 24
@@ -16,7 +17,7 @@ corner_radius_segments = 24
 wall_thickness = 0.9
 floor_thickness = 0.68
 
-labels = True
+add_labels = True
 label_w = 20
 label_h = 6
 
@@ -36,8 +37,6 @@ def frame():
     return linear_extrude(z)(hull()(corners(x,y))) - hole
 
 def label():
-
-
     diagonal = sqrt(pow(label_h,2) / 2)
     label_block = translate([wall_thickness-diagonal,(x-label_w)/2,z-diagonal])(
             rotate([0,45,0])(
@@ -51,7 +50,7 @@ def label():
     return label_block-cutout
 
 def create_labels():
-    if not labels:
+    if not add_labels:
         return union()
     label_objs = union()
     for div_y in range(0, y, divider_spacing_y):
@@ -83,9 +82,48 @@ def divider_x(h=z):
 def box():
     return frame() + create_labels() + dividers(z)
 
-obj = box()
+def usage():
+    print(f'usage: {sys.argv[0]} 100x200x30 2x4')
 
-scad_render_to_file(obj, 'divider_box_generated.scad')
+
+def parse_arguments():
+    import argparse
+    parser = argparse.ArgumentParser(description='Generate SCAD file for a drawer organizer divider box')
+    parser.add_argument('-s','--size', required=True ,type=str, help='The size of the frame in x,y milimeters. format {x}x{y}x{z} e.g: 100x100 or 150x200x50')
+    parser.add_argument('-l','--layout', required=True, type=str, help='The cells layout. format {x_cells}x{y_cells} e.g: 1x2 or 4x4')
+    parser.add_argument('--labels', action='store_true', help='If given will add a panel for label for each cell in the layout')
+    parser.add_argument('--label-size', type=str, help='The size of the label. format {w}x{h} e.g 20x8 default 20x6', default="20x6")
+    parser.add_argument('--corner-radius', type=int, help='Corner radius in milimeters', default=2)
+
+    args = parser.parse_args()
+    
+    global x,y,z,divider_spacing_x,divider_spacing_y, show_labels,label_w,label_h,corner_radius
+    size_sections = args.size.split('x')
+    if len(size_sections) == 3:
+        x,y,z = size_sections
+    elif len(size_sections) == 2:
+        x,y = size_sections
+    else:
+        print("wrong format for size")
+
+    x = int(x)
+    y = int(y)
+    z = int(z)
+    x_cells,y_cells = args.layout.split('x')
+
+    divider_spacing_x = int(x / int(x_cells))
+    divider_spacing_y = int(y / int(y_cells))
+    show_labels = args.labels
+    label_w,label_h = args.label_size.split('x')
+    label_w = int(label_w)
+    label_h = int(label_h)
+    corner_radius = args.corner_radius
+
+    print(f'size: {x}x{y}x{z}\nlayout {x_cells}x{y_cells}\ncell x,y: {divider_spacing_x},{divider_spacing_y}\nlabels: {show_labels} w,h {label_w},{label_h}')
+if __name__ == '__main__':
+    parse_arguments()
+    obj = box()
+    scad_render_to_file(obj, 'divider_box_generated.scad')
 
 
 
